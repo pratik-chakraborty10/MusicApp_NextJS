e1840f3a5f9674bdcf5fcbba08cbf54c5fe11f01
pratik-chakraborty10 <pratikchakraborty3492@gmail.com>
2024-02-22 12:51:39 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "@/components/HeroSection";
+
+
+export default function Home() {
+  return (
+   <main className="min-h-screen bg-black/[0.96] antialised bg-grid-white/[0.02]">
+    
+    <HeroSection/>
+   </main>
+  );
+}
