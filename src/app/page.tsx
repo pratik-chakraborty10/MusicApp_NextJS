e7c89d3d18949b788479bae46f63c794b7459695
pratik-chakraborty10 <pratikchakraborty3492@gmail.com>
2024-02-29 01:50:16 +0530

    instructors and animated tooltip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import UpcomingWebinar from "@/components/UpcomingWebinar";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -14,6 +15,7 @@ export default function Home() {
     <WhyChooseUs/>
     <MusicSchoolTestimonials/>
    <UpcomingWebinar/>
+   <Instructors/>
    </main>
   );
 }
